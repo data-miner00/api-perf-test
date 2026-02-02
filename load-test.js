f05a35b5b884728af2a19de2496407a6b6e37bfd
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-02 16:36:00 +0800

    feat: Improve checks

--- load-test.js
+++ load-test.js
@@ -1,7 +1,19 @@
 import http from "k6/http";
+import { sleep, check } from "k6";
 
 const BASE_URL = false ? "http://host.docker.internal:3000" : "http://app:3000";
 
+export const options = {
+  duration: "5m",
+  vus: 5,
+};
+
 export default function () {
-  http.get(`${BASE_URL}`);
+  let res = http.get(`${BASE_URL}`);
+
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+
+  sleep(Math.random() * 5);
 }

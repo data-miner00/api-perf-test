c47954ff68c354e8e35ce3927851b15c980a3aaa
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-02 15:01:12 +0800

    chore: Containerize app

--- load-test.js
+++ load-test.js
@@ -1,8 +1,6 @@
 import http from "k6/http";
 
-const BASE_URL = true
-  ? "http://host.docker.internal:3000"
-  : "http://localhost:3000";
+const BASE_URL = false ? "http://host.docker.internal:3000" : "http://app:3000";
 
 export default function () {
   http.get(`${BASE_URL}`);

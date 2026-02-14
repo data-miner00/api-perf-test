76108599952dc969108f2d3eab53595853692d91
Shaun Chong <data-miner00@users.noreply.github.com>
2026-02-14 22:01:32 +0800

    feat: Use shared array to pass parameters to api

--- tests/load-test.js
+++ tests/load-test.js
@@ -1,5 +1,6 @@
 import http from "k6/http";
 import { sleep, check } from "k6";
+import { SharedArray } from "k6/data";
 
 const BASE_URL = false ? "http://host.docker.internal:3000" : "http://app:3000";
 
@@ -11,8 +12,25 @@ export const options = {
   ],
 };
 
+const dates = new SharedArray("dates", function () {
+  var dates = [];
+  var currentDate = new Date();
+  var minDate = new Date(currentDate.getTime() - 30 * 24 * 60 * 60 * 1000); // 30 days ago
+
+  for (var i = 0; i < 100; i++) {
+    var randomDate = new Date(
+      minDate.getTime() +
+        Math.random() * (currentDate.getTime() - minDate.getTime()),
+    );
+    dates.push(randomDate.toISOString());
+  }
+
+  return dates;
+});
+
 export default function () {
-  let res = http.get(`${BASE_URL}`);
+  const randomDate = dates[Math.floor(Math.random() * dates.length)];
+  let res = http.get(`${BASE_URL}?date=${randomDate}`);
 
   check(res, {
     "is status 200": (r) => r.status === 200,
